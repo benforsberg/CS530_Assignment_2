d35e1376e56dc71130808a7969ff4a6eb914b551
Ben Forsberg <benj.forsberg@gmail.com>
2020-04-16 13:49:42 -0700

    test of gitignore

---
+++ ChangeScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    //Loads scenes based on order in build settings
+    public void SceneLoader(int SceneIndex)
+	{
+        SceneManager.LoadScene(SceneIndex);
+	}
+
+    //Allows loading of play game scene if globalcontrol bool is true
+    public void PlayGameSceneLoader()
+    {
+		if (GlobalControl.Instance.playGame)
+		{
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    //Quits the program
+    public void Quit()
+    {
+        Debug.Log("Pressed the quit button!"); //System.out.print similar
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+
+        #else
+            Application.Quit();
+
+        #endif
+    }
+}
